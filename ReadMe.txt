d10aaf3f52efab5e037b494eaedea0c8c832649a
sajosam <sajo2023b@mca.ajce.in>
2022-12-01 08:28:47 +0530

    more urls added

--- ReadMe.txt
+++ ReadMe.txt
@@ -14,3 +14,34 @@ http://localhost:8080/api/comments/
 
 #To list individual topics
 http://localhost:8080/api/topics/1
+
+
+# get all topics
+http://localhost:8080/api/get/topics
+
+# get all comments
+http://localhost:8080/api/get/comments
+
+# post topics
+http://localhost:8080/api/post/topics
+
+# post comments
+http://localhost:8080/api/post/comments
+
+#get individual topics
+http://localhost:8080/api/get/topics/1
+
+# get individual comments
+http://localhost:8080/api/get/comments/1
+
+# post individual topics
+http://localhost:8080/api/post/topics/1
+
+# post individual comments
+http://localhost:8080/api/post/comments/1
+
+# get specific comments for a topic
+http://localhost:8080/api/get/topics/1/comments
+
+# post specific comments for a topic
+http://localhost:8080/api/post/topics/1/comments
--- topiccomment/views.py
+++ topiccomment/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
 
+from .models import tbl_topics, tbl_comments
+from .serializers import TopicSerializer, CommentSerializer
+from rest_framework.response import Response
+# from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework import generics, mixins
+from rest_framework import viewsets
 # Create your views here.
 
 from .serializers import TopicSerializer, CommentSerializer
@@ -31,3 +38,77 @@ class individual_comments(generics.ListAPIView):
         return tbl_comments.objects.filter(topic_id=topic_id)
 
 
+
+class gettopic(APIView):
+    def get(self, request, format=None):
+        topic = tbl_topics.objects.all()
+        serializer = TopicSerializer(topic, many=True)
+        return Response(serializer.data)
+
+class posttopic(APIView):
+    def post(self, request, format=None):
+        serializer = TopicSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class getcomments(APIView):
+    def get(self, request, format=None):
+        comments = tbl_comments.objects.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+class postcomments(APIView):
+    def post(self, request, format=None):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class gettopicdetail(APIView):
+    def get(self, request, pk, format=None):
+        topic = tbl_topics.objects.get(pk=pk)
+        serializer = TopicSerializer(topic)
+        return Response(serializer.data)
+
+class getcommentdetail(APIView):
+    def get(self, request, pk, format=None):
+        comment = tbl_comments.objects.get(pk=pk)
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+
+class posttopicdetail(APIView):
+    def post(self, request, pk, format=None):
+        topic = tbl_topics.objects.get(pk=pk)
+        serializer = TopicSerializer(topic, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class postcommentdetail(APIView):
+    def post(self, request, pk, format=None):
+        comment = tbl_comments.objects.get(pk=pk)
+        serializer = CommentSerializer(comment, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class gettopiccomments(APIView):
+    def get(self, request, pk, format=None):
+        comments = tbl_comments.objects.filter(topic_id=pk)
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+class posttopiccomments(APIView):
+    def post(self, request, pk, format=None):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
